f5080e784ea8e4879085d98c15c8930ee185dd87
brunoarsenio625-create <brunoarsenio625@gmail.com>
2025-10-08 10:51:20 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{   int::M,FQ,I,P,media;
+    std::cout<<"isere a nota de Matematica";
+    std:cin>>M;
+     std::cout<<"isere a nota de Fisica Quimica ";                                                                                                                                            
+    std:cin>>F,Q;
+    std::cout<<"isere a nota de Ingles";                                                                                                                                            
+    std:ci>>I;
+    std::cout<<"isere a nota de Portugues";
+    std:cin>>P;
+    
+    
+    media=(P+I+M+F)/4 
+    std::cout"A media deu"<<media<<"."   
+    
+    return 0;
